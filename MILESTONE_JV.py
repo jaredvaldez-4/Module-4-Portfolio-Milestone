e9e81e9a221f079085a0c999280ccfbb91705e68
jaredvaldez-4 <jared.valdez@csuglobal.edu>
2026-01-12 22:39:34 -0700

    Add files via upload

--- MILESTONE_JV.py
+++ MILESTONE_JV.py
@@ -1,26 +1,30 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Mon Jan 7 22:08:53 2026
 
 @author: jared
 """
+item_name = 0
+item_price = 0
+item_quanitity = 0
 
-class ItemToPurchase:
+class ITMTWOPURCHASE:
     def __init__(self):
-        self.item_name = "none"
-        self.item_price = 0
-        self.item_quantity = 0
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quanitity
 
     def print_item_cost(self):
-        total_cost = self.item_price * self.item_quantity
         
-        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${total_cost}")
+        TOTALCOSTITEM = self.item_price * self.item_quantity
+        
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${TOTALCOSTITEM}")
 
 """
 OUTPUT OUTPUT
 """
 
-item1 = ItemToPurchase()
+item1 = ITMTWOPURCHASE()
 
 
 print("---- Item 1 ---- ")
@@ -28,7 +32,7 @@ item1.item_name = input("Please enter the item name:\n")
 item1.item_price = int(input("Now enter the item price:\n"))
 item1.item_quantity = int(input("What is the item quantity:\n"))
 
-item2 = ItemToPurchase()
+item2 = ITMTWOPURCHASE()
 print("\n----  Item 2----  ")
 item2.item_name = input("Please enter the item name:\n")
 item2.item_price = int(input("Now enter the item price:\n"))
@@ -36,9 +40,10 @@ item2.item_quantity = int(input("What is the item quantity:\n"))
 
 
 
-print("\nTOTAL COST")
+print("\n------------- SUMMARY OF TOTAL COST ------------- ")
 item1.print_item_cost()
 item2.print_item_cost()
 
-total = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
-print(f"\nTotal: ${total}")
+GRANDtotal = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+
+print(f"\nGRANDtotal: ${GRANDtotal:}")
